b503826cbc2d7bcde9dbe787af0a56a8a7acdc90
butterbeetle <butterbeetle@naver.com>
2022-04-02 01:47:48 +0900

    Variables const, let, var

---
+++ app.js
@@ -0,0 +1,13 @@
+
+let a = 5; 
+let b = 2; 
+let myName = "KSH";
+
+console.log( a + b );
+console.log( a * b );
+console.log( a / b );
+console.log("hello " + myName);
+
+myName = "KKK";
+
+console.log("hello " + myName);
