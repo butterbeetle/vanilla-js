e5b37520df45fda4b844cf3d1fcf30e731851ac9
butterbeetle <butterbeetle@naver.com>
2022-04-13 00:19:52 +0900

    2.11 return

--- app.js
+++ app.js
@@ -1,28 +1,23 @@
-function sayHello( name, age ){
-    console.log( "My name is " + name + " and my age is " + age );
-}
-
-sayHello("A", 1);
-sayHello("B", 2);
-sayHello("C", 3);
-sayHello("D", 4);
-
-function plus( a, b ){
-    console.log( a + " + " + b + " = " + ( a + b ) );
-}
-function divide( a, b ){
-    console.log( a + " / " + b + " = " + ( a / b ) );
-}
-plus( 1, 3 );
-divide( 3, 6 );
-
-const player = {
-    name: "me",
-    sayHello: function( name ){
-        console.log( "Hello " + name + " nice to meet you");
+const calcultor = {
+    add: function( a, b ){
+        return a+b;
+    },
+    minus: function( a, b ){
+        return a-b;
+    },
+    multiple: function( a, b ){
+        return a*b;
+    },
+    divide: function( a, b ){
+        return a/b;
+    },
+    pow: function( a, b ){
+        return a**b;
     }
-}
+};
 
-console.log( player.name );
-player.sayHello( "AAA" );
-player.sayHello( "BBBB" );
+const plusResult = calcultor.add( 2, 3 );
+const minusResult = calcultor.minus( plusResult, 10 );
+const MutipleResult = calcultor.multiple( 10, minusResult );
+const divideResult = calcultor.divide( MutipleResult, plusResult );
+const powResult = calcultor.pow( divideResult, minusResult );
