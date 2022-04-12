5efa6f2ded492f2160a6a336b5316237f260725f
butterbeetle <butterbeetle@naver.com>
2022-04-12 22:02:16 +0900

    2.7~8 function

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-const player = {
-    name:"nico",
-    points: 10,
-    fat: true,
-};
+function sayHello( name, age ){
+    console.log( "My name is " + name + " and my age is " + age );
+}
 
-player.fat = false;
-player.lastName = "potato";
-console.log( player );
+sayHello("A", 1);
+sayHello("B", 2);
+sayHello("C", 3);
+sayHello("D", 4);
+
+function plus( a, b ){
+    console.log( a + " + " + b + " = " + ( a + b ) );
+}
+function divide( a, b ){
+    console.log( a + " / " + b + " = " + ( a / b ) );
+}
+plus( 1, 3 );
+divide( 3, 6 );
+
+const player = {
+    name: "me",
+    sayHello: function( name ){
+        console.log( "Hello " + name + " nice to meet you");
+    }
+}
+
+console.log( player.name );
+player.sayHello( "AAA" );
+player.sayHello( "BBBB" );
