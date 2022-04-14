118852afc6c74e7884e1b2f5d0dfec82e91130c6
butterbeetle <butterbeetle@naver.com>
2022-04-15 00:17:12 +0900

    3.5 Event

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
-const title = document.getElementById("title");
+const h1 = document.querySelector(".hello:first-child h1");
 
-title.innerText="Got you!";
+console.dir(h1);
 
-console.dir(title);
-console.log(title.id);
-console.log(title.className);
+function handleTitleClick(){
+    console.log( "h1 was clicked" );
+    h1.style.color = "blue";
+}
+
+function handleMouseEnter(){
+    h1.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave(){
+    h1.innerText = "Mouse is gone!";
+}
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy(){
+    alert( "copier!");
+}
+
+function handleWindowOffline(){
+    alert("SOS no WIFI" );
+}
+
+function handleWindowOnline(){
+    alert("ALL GOOD" );
+}
+
+h1.onclick = handleTitleClick;
+h1.onmouseenter = handleMouseEnter;
+h1.addEventListener( "mouseleave", handleMouseLeave );
+
+window.addEventListener( "resize" , handleWindowResize );
+window.addEventListener( "copy" , handleWindowCopy );
+window.addEventListener( "offline" , handleWindowOffline );
+window.addEventListener( "online" , handleWindowOnline );
